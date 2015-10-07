a08be373e25f2cf3a618a66384e4166797a5067d
curtiszimmerman <curtis.zimmerman@gmail.com>
2015-10-07 12:40:49 -0700

    Initial commit: Added boilerplate

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * @project peer2peer
+ * Generic peer-to-peer networking drop-in for Node.js.
+ * @file app.js
+ * Primary application driver
+ * @author curtiszimmmerman
+ * @contact software@curtisz.com
+ * @license AGPLv3
+ * @version 0.0.0
+ */
+
+var __peer2peer = (function() {
+	'use strict';
+
+	return true;
+})();
